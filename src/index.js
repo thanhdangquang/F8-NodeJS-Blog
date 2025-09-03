6b39f8c23b77d40830eca8e062c81ee317e75224
thanhdangquang <kwanthanh2101@gmail.com>
2025-09-04 02:10:27 +0700

    xoa lint-staged,husky

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
 const port = 3000;
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
-const sass = require("sass");
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const sass = require('sass');
 
-const route = require("./routes/index");
+const route = require('./routes/index');
 
 route(app);
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 //HTTP logger
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //template engine setup
 app.engine(
-  "hbs",
-  engine({
-    extname: ".hbs",
-  })
+   'hbs',
+   engine({
+      extname: '.hbs',
+   }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources", "views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+   console.log(`Example app listening on port ${port}`);
 });
